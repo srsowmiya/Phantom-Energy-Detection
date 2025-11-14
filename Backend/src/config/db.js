39df4e32e8183fb0c21074533daee7c04576ebb3
srsowmiya <sowmiyasundaram1040@gmail.com>
2025-11-15 03:08:53 +0530

    backend reviewed

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'phantom_energy_conservation',
   process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || 'Sharvesh',
+  process.env.DB_PASSWORD || 'Sowmiya@1040',
   {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
