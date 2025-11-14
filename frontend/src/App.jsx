a92446d71456ed48c92f251ee58cf192cc3135df
srsowmiya <sowmiyasundaram1040@gmail.com>
2025-11-14 18:26:41 +0530

    React and Tailwind CSS setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+         <h1 className='bg-amber-400'>hello</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
